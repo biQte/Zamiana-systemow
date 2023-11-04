312667070fa78806e572f30d2441baa57f16d9a0
Patryk Gonet <patryk.gonet@microsoft.wsei.edu.pl>
2023-11-04 12:19:25 +0100

    Add project files.

---
+++ Zamiana systemow/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Zamiana_systemow
+{
+    internal class Program
+    {
+        static double convert(string number, int baseNumber)
+        {
+            double result = 0;
+
+            for(int i = 0; i < number.Length; i++)
+            {
+                result += int.Parse(number[i].ToString()) * Math.Pow(baseNumber, number.Length - i - 1);
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            double result = convert("1010", 2);
+            double secondResult = convert("325", 10);
+
+            Console.WriteLine(result);
+            Console.WriteLine(secondResult);
+        }
+    }
+}
